9fb08e1c744fa129a364c73683e3e5d06c9d44ab
Delaney Coveno <delaneycoveno@Delaneys-MacBook-Pro-2.local>
2021-09-10 14:10:51 -0400

    Creates portrait page using gallery items loop

--- src/pages/PortraitPage.tsx
+++ src/pages/PortraitPage.tsx
@@ -1,5 +1,4 @@
 import {ImageList, ImageListItem} from '@material-ui/core'
-import { ClickablePhoto } from '../components/ClickablePhoto';
 import img from '../images/maternity/IMG_7557.jpg'
 import img2 from '../images/christmas tree family/IMG_9378.jpg'
 import img3 from '../images/artists bluff/IMG_1283.jpg'
@@ -8,46 +7,38 @@ import img5 from '../images/maddie cats/IMG_9375.jpg'
 import img6 from '../images/beethoven/IMG_1769.jpg'
 import img7 from '../images/christmas dogs/IMG_9435.jpg'
 
+interface galleryItem {
+    img: string,
+    alt: string,
+    wide?: boolean
+}
 
-// import img2 from '../images/nature/IMG_7064.jpg'
-// import { ClickablePhoto } from '../components/ClickablePhoto'
+/// NEW IDEA: make a config file where can do imports, make gallery  list, manually set WIDE where needed,
+/// manually change order of items on the page easily, then cleanly loop through on the page 
 
 export const PortraitPage = () : JSX.Element => {
-return (
-<>
-        {/* <ImageList rowHeight={160} className={classes.imageList} cols={3}>
-  {itemData.map((item) => (
-    <ImageListItem key={item.img} cols={item.cols || 1}>
-      <img src={item.img} alt={item.title} />
-    </ImageListItem>
-  ))}
-</ImageList> */}
-    <ImageList rowHeight={160} cols={3}>
-        <ImageListItem key={img} rows={4}>
-            <img src={img} width={"fill"} height={"fill"} alt={"blah"}/>
-        </ImageListItem>
-        <ImageListItem key={img} cols={2} rows={4}>
-            <img src={img2} alt={"blah"}/>
-        </ImageListItem>
-        <ImageListItem key={img} rows={4}>
-            <img src={img6} alt={"blah"}/>
-        </ImageListItem>
-        <ImageListItem key={img} rows={4}>
-            <img src={img3} alt={"blah"}/>
-        </ImageListItem>
-        <ImageListItem key={img} rows={4}>
-            <img src={img5} alt={"blah"}/>
-        </ImageListItem>
-        <ImageListItem key={img} rows={4} cols={2}>
-            <img src={img4} alt={"blah"}/>
-        </ImageListItem>
-        <ImageListItem key={img} rows={4}>
-            <img src={img7} alt={"blah"}/>
-        </ImageListItem>
-    </ImageList>
 
-</>
+    //PATTERNS: VW, WV, VVV
+    const g1 : galleryItem = {img: img, alt:"alt1"}
+    const g2: galleryItem = {img: img2, alt: "alt1", wide: true}
+    const g3: galleryItem = {img: img6, alt: "alt"}
+    const g4: galleryItem = {img: img3, alt: "alt"}
+    const g5: galleryItem = {img: img5, alt: "alt"}
+    const g6: galleryItem = {img: img4, alt: "alt", wide: true}
+    const g7: galleryItem = {img: img7, alt: "alt"}
 
-);
+    const gallery : galleryItem[] = [ g1, g2, g3, g4, g5, g6, g7]
 
+    const galleryItems : JSX.Element[] = gallery.map((item) => 
+        <ImageListItem key={item.img} rows={4} cols={item.wide ? 2 : 1}>
+            <img src={item.img} alt={item.alt}/>
+        </ImageListItem>);
+
+    return (
+    <>
+        <ImageList rowHeight={160} cols={3}>
+            {galleryItems}
+        </ImageList>
+    </>
+    )
 }
