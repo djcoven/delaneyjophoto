b8b42bead955c1720c730c8b1daaacda79f909d3
Delaney Coveno <delaneycoveno@Delaneys-MacBook-Pro-2.local>
2021-09-28 21:25:27 -0400

    adds christmas tree family gallery page

--- src/images/christmas tree family/images.tsx
+++ src/images/christmas tree family/images.tsx
@@ -1,6 +1,17 @@
 import { galleryItem } from '../../pages/GalleryFunctions';
 import image01 from './image01.jpg';
+import image02 from './image02.jpg';
+import image03 from './image03.jpg';
+import image04 from './image04.jpg';
+import image05 from './image05.jpg';
+import image06 from './image06.jpg';
+
 
 export const images : galleryItem[] = [
     { img: image01, alt: 'christmas1', wide: true},
+    { img: image04, alt: 'christmas1'},
+    { img: image05, alt: 'christmas1', wide: true},
+    { img: image02, alt: 'christmas1'},
+    { img: image06, alt: 'christmas1'},
+    { img: image03, alt: 'christmas1', wide: true},
   ];
