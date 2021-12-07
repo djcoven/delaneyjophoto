f19ed967ac493131eff2b581f32c696cac570a28
Delaney Coveno <delaneycoveno@Delaneys-MacBook-Pro-2.local>
2021-12-06 21:49:11 -0500

    adds horizontal menu view

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,32 +7,99 @@ type HeaderProps = {
 
 }
 const useStyles = makeStyles({
-    heading : { 
+    appHeader : {
+        backgroundColor: "#D6E5D6",
+        height: "auto",
+        padding: "0px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "5",
+        color: "#7F9680",
+      }, 
+    title : { 
         fontFamily: "Snell Roundhand, cursive",
         fontWeight: "bold",
-        fontSize: "50px",
+        fontSize: "40px",
         padding: "0",
         "&:hover": {
             opacity: ".8"
         },
-        "@media (max-width: 800px)": {
-            paddingTop: "10px",
+        "@media (max-width: 400px)": {
+            paddingTop: "20px",
             fontSize: "20px",
           }
     },
+    subtitle: {
+        paddingBottom: "5px",
+        fontSize: "14px",
+        "@media (max-width: 400px)": {
+            // paddingTop: "20px",
+            fontSize: "10px",
+          }
+    },
+    menu : {
+        backgroundColor: "#bed1be",
+        width: "100%",
+        color: "#7F9680",
+        display: "flex",
+        justifyContent: "space-around",
+        fontWeight: "bold",
+        "@media (max-width: 400px)": {
+            fontSize: "12px",
+          }
+    },
+    menuItem : {
+        paddingTop: "5px",
+        paddingBottom: "5px",
+        width: "25%",
+        height: "100%",
+        "&:hover" : {
+            opacity: 0.8,
+            backgroundColor: "#acc2ac"
+        }
+        }
 })
 
 export const AppHeader = (props: HeaderProps) : JSX.Element => {
     const classes = useStyles();
     return (      
-    <header className="app-header">
+    <header className={classes.appHeader}>
         <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>
-            <div className={classes.heading}>
+            <div className={classes.title}>
                 Delaney Jo Coveno
             </div>
+            <div className={classes.subtitle}> PHOTOGRAPHER </div>
         </Link>
-        <div> Portrait and Nature Photography </div>
-        <AppMenu></AppMenu>
+        <div className={classes.menu}>
+            <span className={classes.menuItem}>
+             <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                 About Me
+             </Link>
+             </span>
+             <span className={classes.menuItem}>
+
+             <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+                 Portraits
+             </Link>
+             </span>
+
+             <span className={classes.menuItem}>
+
+             <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+                 Print Shop
+             </Link>
+             </span>
+
+             <span className={classes.menuItem}>
+
+             <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+                 Contact
+             </Link>
+             </span>
+
+        </div>
     </header>
     )
 }
